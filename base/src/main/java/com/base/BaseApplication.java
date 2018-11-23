b81dd654da6699915ac7ff8d6e8cd19873678c71
mr_wj <1477563131@qq.com>
2018-11-23 17:41:01 +0800

    base模块

---
+++ base/src/main/java/com/base/BaseApplication.java
@@ -0,0 +1,22 @@
+package com.base;
+
+import com.util.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1,1);
+    }
+
+
+}
+
